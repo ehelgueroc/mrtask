9761621cc4da445010d77a31dace6e3708d5966f
Esteban Helguero <esteban.helguero@s4rb.com>
2023-08-12 23:59:41 +0100

    Updated service to add simple find functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      id: 1,
+      name: 'Steve',
+      password: '',
+      createdAt: '',
+      updatedAt: '',
+      deletedAt: '',
+    },
+  ];
+
+  findAll(): any {
+    return this.users;
+  }
+
+  findOne(id: string): any {
+    return this.users.filter((user) => user.id === +id);
+  }
+}
