3de8e8bfb2d74478b2805481f577f2b456a07397
Esteban Helguero <esteban.helguero@s4rb.com>
2023-09-07 22:48:10 +0100

    Created DTOs for users

--- src/users/dtos/register-user.dto.ts
+++ src/users/dtos/register-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsEmail } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
 
 export class RegisterUserDto {
   @IsNotEmpty()
@@ -8,5 +14,10 @@ export class RegisterUserDto {
   email: string;
 
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak',
+  })
   password: string;
 }
