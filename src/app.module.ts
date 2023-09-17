7632e5804f587c3823e64fc6c07f07053ab4d812
Esteban Helguero <esteban.helguero@s4rb.com>
2023-09-17 23:33:58 +0100

    Added validation to the env variables with Joi library

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_SYNCHRONIZE: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
     DatabaseModule,
     AuthModule,
     UsersModule,
